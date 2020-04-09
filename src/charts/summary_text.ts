a6fc56bf1be5894dd61a2711ea7a3b752ff97c3b
Brian M <bmichalski@gmail.com>
2020-04-09 00:19:10 -0400

    Generate metadata dynamically from data!

--- src/charts/summary_text.ts
+++ src/charts/summary_text.ts
@@ -7,13 +7,13 @@ export function summary(state_stats: Array<StateStats>) {
     const recent = state_stats[state_stats.length - 1];
     const prev = state_stats[state_stats.length - 2];
 
-    return `
-    Positive Cases: ${comma(recent.positive)}
-    (${commaSign(recent.positive - prev.positive)}
-    / ${pctChange(prev.positive, recent.positive)}),
-    Confirmed Deaths: ${comma(recent.deaths)}
-    (${commaSign(recent.deaths - prev.deaths)}
-    / ${pctChange(prev.deaths, recent.deaths)})
-    as of ${dateFormat(recent.date)}
-    `;
+    return `\
+Positive Cases: ${comma(recent.positive)} \
+(${commaSign(recent.positive - prev.positive)} \
+/ ${pctChange(prev.positive, recent.positive)}), \
+Confirmed Deaths: ${comma(recent.deaths)} \
+(${commaSign(recent.deaths - prev.deaths)} \
+/ ${pctChange(prev.deaths, recent.deaths)}) \
+as of ${dateFormat(recent.date)}\
+`;
 }
